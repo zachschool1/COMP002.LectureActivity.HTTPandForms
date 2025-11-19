3288cbc3be1d6bfca8c9c08b604c8cf93e7ceb59
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-11-18 21:29:07 -0800

    changed to color picker

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
 
 
-    let input = document.getElementById("text");
-    let inputText = input.value;
-    console.log(inputText);
+    // let input = document.getElementById("text");
+    // let inputText = input.value;
+
+    let color = document.getElementById("colorpicker");
+    let colorValue = color.value;
+    console.log(colorValue);
 
 });
